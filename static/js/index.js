4ad73af761944b840cd760955a0e56b1f671c9d6
dinhtienn <dinhtiennguyen.1202@gmail.com>
2019-05-08 21:09:28 +0700

    full base design

--- static/js/index.js
+++ static/js/index.js
@@ -4,9 +4,26 @@ if (document.querySelector('#information') && document.querySelector('#info-link
 
     infoLink.onmouseover = function() {
         teamInfo.style.opacity = '1';
+        teamInfo.style.top = '25vh';
     }
 
     infoLink.onmouseout = function() {
         teamInfo.style.opacity = '0';
+        teamInfo.style.top = '30vh';
+    }
+}
+
+if (document.querySelector('#tutorial') && document.querySelector('#tut-link')) {
+    var tutorial = document.querySelector('#tutorial');
+    var tutLink = document.querySelector('#tut-link');
+
+    tutLink.onmouseover = function() {
+        tutorial.style.opacity = '1';
+        tutorial.style.top = '25vh';
+    }
+
+    tutLink.onmouseout = function() {
+        tutorial.style.opacity = '0';
+        tutorial.style.top = '30vh';
     }
 }
